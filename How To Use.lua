cbb2e8e5877887f2a36892bb55b8d341674607f2
lost._thur <104391657+SylveriumHub@users.noreply.github.com>
2024-04-19 00:19:07 -0300

    How To Use.lua

---
+++ How To Use.lua
@@ -0,0 +1,12 @@
+--// Create Window
+local Window = SpotifyUiLib:New("Test")
+
+--// Create Buttons
+
+Window:Button("Name Here",function()
+	--// Function Here
+end)
+
+Window:Button("Name Here",function()
+	--// Function Here
+end)
